a55c9e8b0f94d24b8ea042fa0ac57461bf212b4c
tw.maelstrom@gmail.com <trwilson@a8206633e255.ant.amazon.com>
2014-08-27 01:16:02 -0400

    adding index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body
+{
+background-color:#f9f9f9;
+}
+h1
+{
+color:orange;
+text-align:center;
+}
+p
+{
+
+font-size:30px;
+text-align: center;
+}
+
+#content{
+	width: 400px;
+	margin:0px auto;
+	}
+#header{
+	margin-top: 50px;
+	margin-bottom: 50px;
+	}
+
+</style>
+</head>
+	<body>
+		<div id="content">
+			<div id="header"><img src="http://www.awsps.com.s3.amazonaws.com/training/materials/samplefiles/banner.png"></div>
+			<div id="main">
+				<p>
+				<?php
+$url = "http://169.254.169.254/latest/meta-data/instance-id";
+$instance_id = file_get_contents($url);
+echo "Instance ID: <b>" . $instance_id . "</b><br/>";
+$url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+$zone = file_get_contents($url);
+echo "Zone: <b>" . $zone . "</b><br/>";
+?>
+				</p>
+			</div>
+			<div id="footer"> </div>
+		</div>
+	</body>
+</html>
